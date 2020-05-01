7c014727820cffc141d061aef73bc9602739ab4e
damag49 <davidmagyari07@gmail.com>
2020-05-01 22:25:10 +0300

    New items added

---
+++ js/main.js
@@ -0,0 +1,10 @@
+/** Slider */
+$('.slider').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 1500,
+    draggable: false,
+    infinite: false,
+    arrows: false,
+  });
